6224c3d613bea026705006a53b4f10e01ed9344c
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-06-07 13:56:54 +0300

    Add button interaction, create ModelObject script

---
+++ Assets/Scripts/ModelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour
+{
+    public Transform parentOfmodels;
+    public Material defaultMaterial;
+
+    public Transform leftPoint;
+    public Transform centerPoint;
+    public Transform rightPoint;
+
+    private int currentIndex;
+    private List<ModelObject> models = new List<ModelObject>();
+    private string directoryPath = "Assets/Input/";
+    void Start()
+    {
+        models = ModelUploader.UploadModels(directoryPath, rightPoint, parentOfmodels, defaultMaterial);
+        currentIndex = 0;
+        models[currentIndex].ShowModelTo(centerPoint);
+    }
+
+    public void ShowNext()
+    {
+        if(currentIndex< models.Count - 1)
+        {
+            models[currentIndex].HideModelTo(leftPoint);
+            currentIndex++;
+            models[currentIndex].ShowModelTo(centerPoint);
+        }
+    }
+
+    public void ShowPrevious()
+    {
+        if (currentIndex > 0)
+        {
+            models[currentIndex].HideModelTo(rightPoint);
+            currentIndex--;
+            models[currentIndex].ShowModelTo(centerPoint);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ModelObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelObject : MonoBehaviour
+{
+    private float moveSpeed=1.5f;
+    public void HideModelTo(Transform point)
+    {
+        StartCoroutine(MoveCoroutine(point));
+    }
+
+    public void ShowModelTo(Transform point)
+    {
+        StartCoroutine(MoveCoroutine(point));
+    }
+    IEnumerator MoveCoroutine(Transform targerPoint)
+    {
+        while (Vector3.Distance(transform.position, targerPoint.position)>0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targerPoint.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        //yield return new WaitForSeconds(1f);
+    }
+}
