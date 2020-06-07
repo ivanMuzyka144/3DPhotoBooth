bcd56d8a2cda6d2a77fd9569883118de87637915
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-06-07 14:33:21 +0300

    Add changing of alpha value in animation

--- Assets/Scripts/ModelObject.cs
+++ Assets/Scripts/ModelObject.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class ModelObject : MonoBehaviour
 {
     private float moveSpeed=1.5f;
+    private float alphaSpeed = 1.5f;
+    private float currentAlpha = 0;
+    public void SetCurrentColor(Color color)
+    {
+       // currentColor = currentColor;
+    }
     public void HideModelTo(Transform point)
     {
-        StartCoroutine(MoveCoroutine(point));
+        StartCoroutine(MoveCoroutine(point,0));
     }
 
     public void ShowModelTo(Transform point)
     {
-        StartCoroutine(MoveCoroutine(point));
+        StartCoroutine(MoveCoroutine(point, 1));
     }
-    IEnumerator MoveCoroutine(Transform targerPoint)
+    IEnumerator MoveCoroutine(Transform targerPoint, float targetAlpha)
     {
-        while (Vector3.Distance(transform.position, targerPoint.position)>0.05f)
+        while (Vector3.Distance(transform.position, targerPoint.position)>0.1f
+               || Mathf.Abs(currentAlpha - targetAlpha) > 0.01f)
         {
+            if(currentAlpha> targetAlpha)
+            {
+                currentAlpha -= alphaSpeed * Time.deltaTime;
+            }
+            else
+            {
+                currentAlpha += alphaSpeed * Time.deltaTime;
+            }
+            SetAlphaToChildren(currentAlpha);
             transform.position = Vector3.Lerp(transform.position, targerPoint.position, moveSpeed * Time.deltaTime);
             yield return null;
         }
         //yield return new WaitForSeconds(1f);
     }
+
+    private void SetAlphaToChildren(float alphaValue)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            Color childColor = child.GetComponent<MeshRenderer>().material.color;
+            child.GetComponent<MeshRenderer>().material.color = new Color(childColor.r, childColor.g, childColor.b, alphaValue);
+        }
+    }
+
 }
